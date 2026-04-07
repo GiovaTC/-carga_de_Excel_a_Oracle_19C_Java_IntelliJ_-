aa86e653ee5471aae7354d278590f2d19a19e3a1
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-07 13:59:21 -0500

    commit - ExcelData .

---
+++ src/ExcelData.java
@@ -0,0 +1,19 @@
+public class ExcelData {
+
+    private String columna1;
+    private String columna2;
+    private String columna3;
+    private String columna4;
+
+    public ExcelData(String c1, String c2, String c3, String c4) {
+        columna1 = c1;
+        columna2 = c2;
+        columna3 = c3;
+        columna4 = c4;
+    }
+
+    public String getColumna1() { return columna1; }
+    public String getColumna2() { return columna2; }
+    public String getColumna3() { return columna3; }
+    public String getColumna4() { return columna4; }
+}
