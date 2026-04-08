78b4715c928e49fac38b1e68945c155f0b561027
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-08 10:21:17 -0500

    commit - Controlador .

--- src/Controlador.java
+++ src/Controlador.java
@@ -12,12 +12,39 @@ public class Controlador {
         this.vista = vista;
         this.service = new ExcelService();
         this.dao = new ExcelDAO();
-        
+
+        // 🔹 Generar archivo de ejemplo automáticamente
+        GeneradorExcel.crearArchivoEjemplo("datos_ejemplo.csv");
+
         vista.btnCargar.addActionListener(this::cargarExcel);
     }
 
-    private void cargarExcel(ActionEvent actionEvent) {
+    private void cargarExcel(ActionEvent e) {
+
+        JFileChooser fileChooser = new JFileChooser();
+        int opcion = fileChooser.showOpenDialog(null);
+
+        if (opcion == JFileChooser.APPROVE_OPTION) {
+
+            String ruta = fileChooser.getSelectedFile().getAbsolutePath();
+
+            List<ExcelData> lista = service.leerExcel(ruta);
+
+            vista.modelo.setRowCount(0);
+
+            for (ExcelData d : lista) {
+
+                vista.modelo.addRow(new Object[]{
+                        d.getColumna1(),
+                        d.getColumna2(),
+                        d.getColumna3(),
+                        d.getColumna4()
+                });
+
+                dao.insertar(d);
+            }
+
+            JOptionPane.showMessageDialog(null, "Datos cargados y guardados en Oracle");
+        }
     }
-
-
 }
